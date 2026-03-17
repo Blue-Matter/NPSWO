1a6b4e813eaa8e86552255bff1f1d500ddbde999
AdrianHordyk <ar.hordyk@gmail.com>
2026-03-16 23:50:50 -0700

    Update 0. Specifications.R

--- 0. Specifications.R
+++ 0. Specifications.R
@@ -11,10 +11,7 @@ Interval <- 1  # number of years between management updates
 DataLag  <- 0  # years of lag between data collection and management action (to be updated)
 pYear    <- 25 # number of years in the MSE projection period
 
-# Path to the SS3 base-case model directory. Life-history parameters in this
-# model will be modified across the ensemble of SS3 models;
-# all other structural settings are inherited as-is unless modified elsewhere in
-# this script.
+# Path to the SS3 base-case model directory
 ssdir_base <- "../WCNPOSWO-2023/Final Base-case"
 
 # Detect available cores and register a parallel backend for downstream functions
